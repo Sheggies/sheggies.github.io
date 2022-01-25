4634004f13e43634220540abb03ffdc9ae40b3d1
Sheggy <8018511+Sheggies@users.noreply.github.com>
2022-01-25 02:11:08 +0100

    Fix import

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as MathUtil from "./modules/math/math_module.js";
-import Canvas from "./modules/canvas/canvas_module.js";
+import * as CanvasModule from "./modules/canvas/canvas_module.js";
 
 let v = new MathUtil.Vector2(1, 2);
-let c = new Canvas(0, document.body, 400, 400);
+let c = new CanvasModule.Canvas(0, document.body, 400, 400);
