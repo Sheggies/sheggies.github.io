6673ad4a5fcd2e20d354a8693765e7f3d0a59633
Sheggy <8018511+Sheggies@users.noreply.github.com>
2022-01-25 15:31:43 +0100

    Simplify imports

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import * as MathUtil from "./modules/math/math_module.js";
-import * as CanvasModule from "./modules/canvas/canvas_module.js";
+import { Vector2 } from "./modules/math/math_module.js";
+import { Canvas, Circle } from "./modules/canvas/canvas_module.js";
 
-let v = new MathUtil.Vector2(1, 2);
-let c = new CanvasModule.Canvas(0, document.getElementById("main"), 400, 400);
+let v = new Vector2(1, 2);
+let c = new Canvas(0, document.getElementById("main"), 400, 400);
 
-let circle = new CanvasModule.Circle(c.renderingContext, v, 10, "green");
+let circle = new Circle(c.renderingContext, v, 10, "green");
 circle.draw();
