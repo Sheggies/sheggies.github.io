e2a537b9c08ca70001fc64aee171c52aa009436d
Sheggy <8018511+Sheggies@users.noreply.github.com>
2022-01-25 02:14:34 +0100

    Test primitive drawing

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ import * as CanvasModule from "./modules/canvas/canvas_module.js";
 
 let v = new MathUtil.Vector2(1, 2);
 let c = new CanvasModule.Canvas(0, document.body, 400, 400);
+
+let circle = new CanvasModule.Circle(c.renderingContext, v, 10, "green");
+circle.draw();
