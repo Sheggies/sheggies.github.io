eb73bc4ea7232546333bb728496b2a6bb39ec40f
Sheggy <8018511+Sheggies@users.noreply.github.com>
2022-01-25 15:24:17 +0100

    Change canvas parent element

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as MathUtil from "./modules/math/math_module.js";
 import * as CanvasModule from "./modules/canvas/canvas_module.js";
 
 let v = new MathUtil.Vector2(1, 2);
-let c = new CanvasModule.Canvas(0, document.body, 400, 400);
+let c = new CanvasModule.Canvas(0, document.getElementById("main"), 400, 400);
 
 let circle = new CanvasModule.Circle(c.renderingContext, v, 10, "green");
 circle.draw();
